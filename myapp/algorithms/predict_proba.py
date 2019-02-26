a4680f64fdd740f7ab6b90c54e72cc74334b6aef
raoniz <rahulvsoni1997@gmail.com>
2019-02-26 14:20:55 +0530

    Fixed path error

--- myapp/algorithms/predict_proba.py
+++ myapp/algorithms/predict_proba.py
@@ -1,4 +1,5 @@
 import pickle
+import numpy as np
 
 def main(filename, test):
     le = pickle.load(open('../data/pickles/label_fit.pkl', 'rb'))
@@ -11,4 +12,4 @@ def main(filename, test):
     return newmodel.predict_proba(test)[0][1] * 100
 
 if __name__ == '__main__':
-    print(main('vjti.pkl'))
+    print(main('../data/pickle/vjti.pkl', np.array(input('Enter test data: ').strip().split(' '))))
