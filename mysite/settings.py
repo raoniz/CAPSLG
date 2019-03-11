95b9dce92851827a1e905286c2baad92469ba551
raoniz <rahulvsoni1997@gmail.com>
2019-03-11 20:03:52 +0530

    Fix static files issue

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,5 +126,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
